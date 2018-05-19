cf32be1ccdf515d792758c61481eefefb92809cb
Marek Dudek <marek.dudek@gmail.com>
2018-05-19 18:26:50 +0200

    common trait for synch and asynch terminal

--- src/main/scala/tutorial/introduction/AbstractingOverExecution.scala
+++ src/main/scala/tutorial/introduction/AbstractingOverExecution.scala
@@ -6,23 +6,25 @@ import scala.language.higherKinds
 object AbstractingOverExecution {
 }
 
-package object SomeObject {
-  type EitherString[T] = Either[String, T]
-  type Id[T] = T
-}
 
+// two traits for single functionality
 
 trait TerminalSync {
+
   def read(): String
 
   def write(t: String): Unit
 }
 
 trait TerminalAsynch {
+
   def read(): Future[String]
 
   def write(t: String): Future[Unit]
 }
+
+
+// higher-kinded types
 
 trait Foo[C[_]] {
   def create(i: Int): C[Int]
@@ -32,6 +34,13 @@ object FooList extends Foo[List] {
   def create(i: Int): List[Int] = List(i)
 }
 
+// type alias trick
+
+package object SomeObject {
+  type EitherString[T] = Either[String, T]
+  type Id[T] = T
+  type Now[X] = X
+}
 
 object FooEither extends Foo[SomeObject.EitherString] {
   def create(i: Int): SomeObject.EitherString[Int] = Right(i)
@@ -41,3 +50,42 @@ object FooId extends Foo[SomeObject.Id] {
   def create(i: Int): Int = i
 }
 
+// single train for functionality
+
+trait Terminal[C[_]] {
+
+  def read(): C[String]
+
+  def write(t: String): C[Unit]
+}
+
+object TerminalSync extends Terminal[SomeObject.Now] {
+
+  override def read(): String = ???
+
+  override def write(t: String): Unit = ???
+}
+
+object TerminalAsynch extends Terminal[Future] {
+
+  override def read(): Future[String] = ???
+
+  override def write(t: String): Future[Unit] = ???
+}
+
+trait Execution[C[_]] {
+
+  def doAndThen[A, B](c: C[A])(f: A => C[B]): C[B]
+
+  def create[B](b: B): C[B]
+}
+
+package object Some2 {
+
+  def echo[C[_]](t: Terminal[C], e: Execution[C]): C[String] =
+    e.doAndThen(t.read) { in: String =>
+      e.doAndThen(t.write(in)) { _: Unit =>
+        e.create(in)
+      }
+    }
+}
