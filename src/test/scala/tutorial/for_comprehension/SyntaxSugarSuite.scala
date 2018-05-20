7a29a97b2d862926a531158a2c7fecab2f6f6cf9
Marek Dudek <marek.dudek@gmail.com>
2018-05-20 21:28:47 +0200

    desugared nested for-comprehension

--- src/test/scala/tutorial/for_comprehension/SyntaxSugarSuite.scala
+++ src/test/scala/tutorial/for_comprehension/SyntaxSugarSuite.scala
@@ -10,12 +10,23 @@ class SyntaxSugarSuite extends FunSuite {
     val b = Option(2)
     val c = Option(3)
     // when
-    val r = for {
+    val r1 = for {
       i <- a
       j <- b
       k <- c
     } yield i + j + k
+    // when
+    val r2 = a.flatMap {
+      i =>
+        b.flatMap {
+          j =>
+            c.map {
+              k => i + j + k
+            }
+        }
+    }
     // then
-    assert(r.contains(6))
+    assert(r1 == r2)
+    assert(r1.contains(6))
   }
 }
