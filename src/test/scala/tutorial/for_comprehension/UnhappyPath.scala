9b3196f7a489f932f3db726bdfa9ba45f238c026
Marek Dudek <marek.dudek@gmail.com>
2018-05-23 22:29:47 +0200

    for-comprehension short-circuits for either and future

--- src/test/scala/tutorial/for_comprehension/UnhappyPath.scala
+++ src/test/scala/tutorial/for_comprehension/UnhappyPath.scala
@@ -2,14 +2,35 @@ package tutorial.for_comprehension
 
 import org.scalatest.FunSuite
 
+import scala.concurrent.{Await, Future, duration}
+
 class UnhappyPath extends FunSuite {
 
-  test("unhappy path for option") {
-    val v = for {
-      i <- Option(1)
-    } yield i
-    assert(
-       v == Option(1)
-    )
+  test("unhappy path for Either") {
+    // when
+    val res = for {
+      i <- Right(1)
+      j <- Left("a"): Either[String, Int]
+      k <- Left("b"): Either[String, Int]
+    } yield i + j + k
+    // then
+    assert(res == Left("a"))
+  }
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  test("unhappy path for Future") {
+    // when
+    val res = for {
+      i <- Future.failed[Int](new Throwable)
+      j <- Future {
+        println("hello")
+        1
+      }
+    } yield i + j
+    // then
+    assertThrows[Throwable] {
+      Await.result(res, duration.Duration.Inf)
+    }
   }
 }
