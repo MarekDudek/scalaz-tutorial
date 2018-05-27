b306959fdf8d8712329ccb662474319a508f15d0
Marek Dudek <marek.dudek@gmail.com>
2018-05-27 22:09:54 +0200

    act method

--- src/main/scala/tutorial/application_design/algebra/business_logic.scala
+++ src/main/scala/tutorial/application_design/algebra/business_logic.scala
@@ -35,9 +35,58 @@ final class DynAgents[F[_]]
       update = snap.copy(pending = pending)
     } yield update
 
+  def act(world: WorldView): F[WorldView] =
+    world match {
+      case NeedsAgent(node) =>
+        for {
+          _ <- m.start(node)
+          update = world.copy(pending = Map(node -> world.time))
+        } yield update
+      case Stale(nodes) =>
+        nodes.foldLeftM(world) {
+          (world, n) =>
+            for {
+              _ <- m.stop(n)
+              update = world.copy(pending = world.pending + (n -> world.time))
+            } yield update
+        }
+      case _ => world.pure[F]
+    }
+
   private def symdiff[T](a: Set[T], b: Set[T]): Set[T] =
     (a union b) -- (a intersect b)
 
   private def timediff(from: Instant, to: Instant): FiniteDuration =
     ChronoUnit.MINUTES.between(from, to).minutes
+
+  private object NeedsAgent {
+    def unapply(world: WorldView): Option[MachineNode] =
+      world match {
+        case WorldView(backlog, 0, managed, alive, pending, _)
+          if backlog > 0 && alive.isEmpty && pending.isEmpty
+        => Option(managed.head)
+        case _ => None
+      }
+  }
+
+  private object Stale {
+    def unapply(world: WorldView): Option[NonEmptyList[MachineNode]] =
+      world match {
+        case WorldView(backlog, _, _, alive, pending, time)
+          if alive.nonEmpty =>
+          (alive -- pending.keys).collect {
+            case (n, started)
+              if backlog == 0 && timediff(started, time).toMinutes % 60 >= 58 =>
+              n
+            case (n, started)
+              if timediff(started, time) >= 5.hours =>
+              n
+          }.toList.toNel
+        case _ => None
+      }
+  }
+
 }
+
+
+
