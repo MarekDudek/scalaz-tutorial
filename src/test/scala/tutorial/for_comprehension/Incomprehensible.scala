887fc3b42a732174b132641854cf643fd5a39e14
Marek Dudek <marek.dudek@gmail.com>
2018-05-27 18:03:09 +0200

    monad transformers example

--- src/test/scala/tutorial/for_comprehension/Incomprehensible.scala
+++ src/test/scala/tutorial/for_comprehension/Incomprehensible.scala
@@ -2,10 +2,11 @@ package tutorial.for_comprehension
 
 import org.scalatest.FunSuite
 import org.scalatest.Matchers._
+import scalaz.OptionT
 import scalaz.Scalaz._
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future, duration}
 
 class Incomprehensible extends FunSuite {
 
@@ -25,5 +26,34 @@ class Incomprehensible extends FunSuite {
         b <- future
     } yield a * b
     """ shouldNot compile
+  }
+
+  def futureOption1: Future[Option[Int]] =
+    2.pure[Option].pure[Future]
+
+  def futureOption2: Future[Option[Int]] =
+    3.pure[Option].pure[Future]
+
+  test("monad transformers") {
+    // when
+    val transformer: OptionT[Future, Int] =
+      for {
+        a <- OptionT(futureOption1)
+        b <- OptionT(futureOption2)
+      } yield a * b
+    val futureMaybeResult: Future[Option[Int]] = transformer.run
+    val maybeResult = Await.result(futureMaybeResult, duration.Duration.Inf)
+    // then
+    assert(maybeResult.contains(6))
+    // when
+    val futureResult: Future[Int] = transformer getOrElse 0
+    val result = Await.result(futureResult, duration.Duration.Inf)
+    // then
+    assert(result == 6)
+    // when
+    val futureResult2: Future[Int] = transformer getOrElseF 0.pure[Future]
+    val result2 = Await.result(futureResult2, duration.Duration.Inf)
+    // then
+    assert(result2 == 6)
   }
 }
