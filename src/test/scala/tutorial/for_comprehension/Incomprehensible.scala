1be4eb33a40954ffc136a4836e1114b67bf7a7f8
Marek Dudek <marek.dudek@gmail.com>
2018-05-27 18:50:01 +0200

    mixing methods returning different monads with DSL and thrush operator

--- src/test/scala/tutorial/for_comprehension/Incomprehensible.scala
+++ src/test/scala/tutorial/for_comprehension/Incomprehensible.scala
@@ -56,4 +56,58 @@ class Incomprehensible extends FunSuite {
     // then
     assert(result2 == 6)
   }
+
+  test("mixing monad transformers with method returning plain outer monad") {
+    // when
+    val transformer =
+      for {
+        a <- OptionT(futureOption1)
+        b <- OptionT(futureOption2)
+        c <- future.liftM[OptionT]
+      } yield a * b / c
+    val futureResult: Future[Int] = transformer getOrElse 0
+    // then
+    val result = Await.result(futureResult, duration.Duration.Inf)
+    assert(result == 2)
+  }
+
+  test("mixing monad transformers with method returning plain inner monad") {
+    // when
+    val transformer =
+      for {
+        a <- OptionT(futureOption1)
+        b <- OptionT(futureOption2)
+        c <- future.liftM[OptionT]
+        d <- OptionT(option.pure[Future])
+      } yield (a * b) / (c * d)
+    val futureResult: Future[Int] = transformer getOrElse 0
+    // then
+    val result = Await.result(futureResult, duration.Duration.Inf)
+    assert(result == 1)
+  }
+
+  def liftFutureOption[A](f: Future[Option[A]]): OptionT[Future, A] = OptionT(f)
+
+  def liftFuture[A](f: Future[A]): OptionT[Future, A] = f.liftM[OptionT]
+
+  def liftOption[A](o: Option[A]): OptionT[Future, A] = OptionT(o.pure[Future])
+
+  def lift[A](a: A): OptionT[Future, A] = liftOption(Option(a))
+
+  test("mixing monad transformers with methods returning plain monads with DSL") {
+    // given
+    val transformer: OptionT[Future, Int] =
+      for {
+        a <- futureOption1 |> liftFutureOption
+        b <- futureOption2 |> liftFutureOption
+        c <- future |> liftFuture
+        d <- option |> liftOption
+        e <- 10 |> lift
+      } yield e * (a * b) / (c * d)
+    // then
+    val futureResult: Future[Int] = transformer getOrElse 0
+    // then
+    val result = Await.result(futureResult, duration.Duration.Inf)
+    assert(result == 10)
+  }
 }
