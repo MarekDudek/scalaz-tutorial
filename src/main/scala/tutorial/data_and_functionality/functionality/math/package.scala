396757bf443638e2da7657c1b4e178d86e43f9c9
Marek Dudek <marek.dudek@gmail.com>
2018-06-02 17:03:10 +0200

    functionality - implicits'

---
+++ src/main/scala/tutorial/data_and_functionality/functionality/math/package.scala
@@ -0,0 +1,7 @@
+package tutorial.data_and_functionality.functionality
+
+package object math {
+
+  def sin(x: Double): Double = java.lang.Math.sin(x)
+}
+
---
+++ src/test/scala/tutorial/data_and_functionality/FunctionalitySuite.scala
@@ -0,0 +1,47 @@
+package tutorial.data_and_functionality
+
+import java.lang.Math.PI
+
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.language.implicitConversions
+
+
+class FunctionalitySuite extends FlatSpec with Matchers {
+
+  "Functions" should "or are typically defined on an object" in {
+    functionality.math.sin(PI / 2) shouldBe 1.0
+  }
+
+  implicit class DoubleOps(x: Double) {
+    def sin: Double = java.lang.Math.sin(x)
+  }
+
+  "implicit class" should "give us better style" in {
+    (PI / 2).sin shouldBe 1.0
+  }
+
+  implicit def DoubleOps2(x: Double): DoubleOps2 =
+    new DoubleOps2(x)
+
+  class DoubleOps2(x: Double) {
+    def sin2: Double = java.lang.Math.sin(x)
+  }
+
+  "implicit class" should "be only syntax sugar for implicit conversion" in {
+    (PI / 2).sin2 shouldBe 1.0
+  }
+
+  "this version" should "avoid allocations" in {
+    import ImplicitHolder._
+    (PI/ 2).sin3 shouldBe 1.0
+  }
+}
+
+object ImplicitHolder {
+
+  implicit final class DoubleOps3(val x: Double) extends AnyVal {
+    def sin3: Double = java.lang.Math.sin(x)
+  }
+
+}
